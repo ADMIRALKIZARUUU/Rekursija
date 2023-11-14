6de010e47208bd89c1493b28e5b1ff436127bc6e
GromGrom <GromGrom@WIN-RQAPC8EIA9A>
2023-11-14 23:18:27 +0200

    Добавьте файлы проекта.

---
+++ Rekursija/Rekursija.cpp
@@ -0,0 +1,52 @@
+﻿// Rekursija.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int uzd(int n) {
+	
+	if (n == 0) {
+		return 0;
+
+	}
+	else {
+		return n % 10 + uzd(n/10);
+	}
+}
+
+int fibonaci(int n) {
+
+	if (n==1) {
+		return n;
+	}
+
+	if (n==0) {
+		return n;
+	}
+
+	else {
+		return fibonaci(n-1) + fibonaci(n-2);
+
+
+	}
+}
+
+// void rek(int a) {	
+//	for (int i = 0; i <= a; i++) {
+//		cout << fibonaci(i) << " ";
+//	}
+
+//}
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cout << "Введите N чисел Фибоначи:";
+	cin >> a;
+	//cout << fibonaci(a) << " ";
+	cout << uzd(a);
+	// rek(a);
+
+}
+
